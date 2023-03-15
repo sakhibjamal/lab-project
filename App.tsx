e881346f6369e1e21543d52572af997240a23420
sakhibjamal <osakhibjamal@gmail.com>
2023-03-15 14:04:35 +0500

    project init

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Init Project</Text>
+    </View>
+  );
+}
+
+export default App;
